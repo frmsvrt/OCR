95750a4a506e51afee4bb1b303a403768b204723
Kravchenko Fyodor <udalite.feda@hotmail.com>
2019-02-11 22:11:05 +0800

    update .gitignore

--- models/crnn.py
+++ models/crnn.py
@@ -1,76 +1,69 @@
+import torch
 import torch.nn as nn
 
+class ConvRelu(nn.Module):
+    def __init__(self, nin, nout, k, s, p, bn=False):
+        super().__init__()
+        self.conv = nn.Conv2d(nin, nout,
+                              kernel_size=k,
+                              stride=s,
+                              padding=p)
+        self.activation = nn.ReLU(inplace=True)
+        self.bn = nn.BatchNorm2d(nout)
+        self.bn_ = bn
 
-class BILSTM(nn.Module):
-    def __init__(self, nin, nHidden, nOut):
-        super(BILSTM, self).__init__()
-        self.rnn = nn.LSTM(nin, nHidden, bidirectional=True)
-        self.embedding = nn.Linear(nHidden * 2, nOut)
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn_:
+            x = self.bn(x)
+        x = self.activation(x)
+        return x
+
+class LSTM(nn.Module):
+    def __init__(self, nin, nh, nout):
+        super(LSTM, self).__init__()
+        self.rnn = nn.LSTM(nin, nh, bidirectional=True)
+        self.embedding = nn.Linear(nh * 2, nout)
+        if torch.cuda.is_available():
+            self.ngpu = torch.cuda.device_count()
 
     def forward(self, input):
-        recurrent, _ = self.rnn(input)
+        recurrent, _ = data_parallel(
+            self.rnn, input, self.ngpu)
         T, b, h = recurrent.size()
         t_rec = recurrent.view(T * b, h)
 
-        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = self.embedding(t_rec)
         output = output.view(T, b, -1)
 
         return output
 
-
 class CRNN(nn.Module):
-    def __init__(self, imgH, nc, nclass, nh, n_rnn=2, leakyRelu=False):
+    def __init__(self, c, nc, nh):
         super(CRNN, self).__init__()
-        assert imgH % 16 == 0, 'imgH has to be a multiple of 16'
+        layers = [ConvRelu(c, 64, 3, 1, 1),
+                 nn.MaxPool2d(2,2),
+                 ConvRelu(64, 128, 3, 1, 1),
+                 nn.MaxPool2d(2,2),
+                 ConvRelu(128, 256, 3, 1, 1, True),
+                 ConvRelu(256, 256, 3, 1, 1),
+                 nn.MaxPool2d((2,2),(2,1),(0,1)),
+                 ConvRelu(256, 512, 3, 1, 1, True),
+                 ConvRelu(512, 512, 3, 1, 1),
+                 nn.MaxPool2d((2,2), (2,1), (0,1)),
+                 ConvRelu(512, 512, 2, 1, 0, True),
+                 ]
 
-        ks = [3, 3, 3, 3, 3, 3, 2]
-        ps = [1, 1, 1, 1, 1, 1, 0]
-        ss = [1, 1, 1, 1, 1, 1, 1]
-        nm = [64, 128, 256, 256, 512, 512, 512]
+        self.cnn = nn.Sequential(*layers)
+        self.rnn = nn.Sequential(LSTM(512, nh, nh),
+                                LSTM(nh, nh, nc))
 
-        cnn = nn.Sequential()
-
-        def convRelu(i, batchNormalization=False):
-            nin = nc if i == 0 else nm[i - 1]
-            nout = nm[i]
-            cnn.add_module('conv{0}'.format(i),
-                           nn.Conv2d(nin, nout, ks[i], ss[i], ps[i]))
-            if batchNormalization:
-                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nout))
-            if leakyRelu:
-                cnn.add_module('relu{0}'.format(i),
-                               nn.LeakyReLU(0.2, inplace=True))
-            else:
-                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
-
-        convRelu(0) #1
-        cnn.add_module('pooling{0}'.format(0), nn.MaxPool2d(2, 2))  # 64x16x64
-        convRelu(1) #2
-        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 2))  # 128x8x32
-        convRelu(2, True) #3
-        convRelu(3) #4
-        cnn.add_module('pooling{0}'.format(2),
-                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 256x4x16
-        convRelu(4, True) #5
-        convRelu(5) #6
-        cnn.add_module('pooling{0}'.format(3),
-                       nn.MaxPool2d((2, 2), (2, 1), (0, 1)))  # 512x2x16
-        convRelu(6, True)  # 512x1x16 #7
-
-        self.cnn = cnn
-        self.rnn = nn.Sequential(
-            BILSTM(512, nh, nh),
-            BILSTM(nh, nh, nclass))
-
-    def forward(self, input):
-        # conv features
-        conv = self.cnn(input)
-        b, c, h, w = conv.size()
-        assert h == 1, "the height of conv must be 1"
-        conv = conv.squeeze(2)
-        conv = conv.permute(2, 0, 1)  # [w, b, c]
-
-        # rnn features
-        output = self.rnn(conv)
-
-        return output
+    def forward(self, x):
+        x = self.cnn(x)
+        print(x.size())
+        b, c, h, w = x.size()
+        x = x.squeeze(2)
+        x = x.permute(2, 0, 1)
+        x = self.rnn(x)
+        print(x.size())
+        return x
